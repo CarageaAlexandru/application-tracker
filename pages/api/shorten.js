584c017c008b70c1fe4be37a5223a0c57c6978e4
Caragea <caragea.alexandru@yahoo.com>
2024-02-21 18:41:20 +0000

    save urls into the db

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,8 +1,8 @@
 // pages/api/shorten.js
 import { nanoid } from "nanoid";
-import { Pool } from "pg";
+import { createClient } from "@vercel/postgres";
 
-const pool = new Pool({
+const pg = createClient({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false,
@@ -13,13 +13,11 @@ export default async function handler(req, res) {
 	if (req.method === "POST") {
 		const url = req.body.url;
 		const id = nanoid(5);
-		await pool.query("INSERT INTO urls (id, url) VALUES ($1, $2)", [id, url]);
+		await pg.query("INSERT INTO urls (id, url) VALUES ($1, $2)", [id, url]);
 		res.status(200).json({ id });
 	} else if (req.method === "GET") {
 		const id = req.query.id;
-		const { rows } = await pool.query("SELECT url FROM urls WHERE id = $1", [
-			id,
-		]);
+		const { rows } = await pg.query("SELECT url FROM urls WHERE id = $1", [id]);
 		const url = rows[0]?.url;
 		if (url) {
 			res.redirect(url);
